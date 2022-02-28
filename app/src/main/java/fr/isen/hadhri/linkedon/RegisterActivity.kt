9b74a0c4775819553e50a5b31b6e8370a54ec151
PierreTinco <pierre.tinco@isen.yncrea.fr>
2022-02-28 15:58:15 +0100

    login/authentication

--- app/src/main/java/fr/isen/hadhri/linkedon/RegisterActivity.kt
+++ app/src/main/java/fr/isen/hadhri/linkedon/RegisterActivity.kt
@@ -10,17 +10,18 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import fr.isen.hadhri.linkedon.databinding.ActivityLoginBinding
+import fr.isen.hadhri.linkedon.databinding.ActivityRegisterBinding
 
-class LoginActivity : AppCompatActivity() {
-    private  lateinit var binding: ActivityLoginBinding
+class RegisterActivity : AppCompatActivity() {
+    private  lateinit var binding: ActivityRegisterBinding
     private lateinit var auth: FirebaseAuth
-    val TAG = "LoginActivity"
+    val TAG = "RegisterActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val database = Firebase.database("https://linkedon-b7c62-default-rtdb.europe-west1.firebasedatabase.app")
         val TAG = "LoginActivity"
         val myRef = database.getReference("post")
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
         auth = Firebase.auth
         listenClick()
@@ -33,21 +34,20 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun listenClick() {
-        binding.LoginBtn.setOnClickListener {
-            auth.createUserWithEmailAndPassword(binding.EmailAddress.text.toString(),binding.Password.text.toString())
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "createUserWithEmail:success")
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                        Log.w(binding.EmailAddress.text.toString(), task.exception)
-                        Toast.makeText(baseContext, "Authentication failed.",
-                            Toast.LENGTH_SHORT).show()
-                    }
+        auth.signInWithEmailAndPassword(binding.EmailAddress.text.toString(),binding.Password.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+
                 }
-        }
+            }
     }
 
     private fun updateUI(user: FirebaseUser?) {
