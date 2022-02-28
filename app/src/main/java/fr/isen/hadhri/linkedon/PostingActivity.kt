59ba13d4ffe1823fc4e86bfde6f8bd940a45d54c
Johanna Corre <johanna.corre@isen.yncrea.fr>
2022-02-28 16:55:29 +0100

    posting form

---
+++ app/src/main/java/fr/isen/hadhri/linkedon/PostingActivity.kt
@@ -0,0 +1,19 @@
+package fr.isen.hadhri.linkedon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_posting)
+
+        //récupérer le pseudo/nom&prénom de la personne qui poste à travers des informations de connexion
+
+        //le bouton post renvoie sur les détails du post en mode lecture
+
+        //nom&prénom/pseudo, date et heure de post, titre, contenu du post, bouton like, bouton commentaire
+
+        //si possible écrire en texte qui se remplacera "écrivez votre message ici" etc
+    }
+}
